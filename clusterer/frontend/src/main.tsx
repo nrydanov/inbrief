88470de214d7bc15075538b663bbbea5438577bd
Nikita Rydanov <find.art.in.living@gmail.com>
2025-05-25 13:04:42 +0400

    feat(frontend): Added Inter

--- clusterer/frontend/src/main.tsx
+++ clusterer/frontend/src/main.tsx
@@ -1,11 +1,21 @@
+import '@fontsource-variable/inter';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './theme.css'
 import './index.css'
 import App from './App.tsx'
 
+const theme = createTheme({
+  typography: {
+    fontFamily: ['"Inter Variable"', 'sans-serif'].join(','),
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
